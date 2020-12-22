7fd620091e36bc0e07598dd3ba686fdffead2248
Chris Walsh <chris@offwhitehouse.co.uk>
2020-12-22 12:08:33 +0000

    Day 18

---
+++ 18_b.py
@@ -0,0 +1,42 @@
+from printf import printf
+import copy
+import re 
+
+# 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+def evaluate(s):
+  while ' ' in s:
+    if '(' in s:
+      match = re.search('\(([0-9 \+\*]*)\)', s)
+      sub_clause = match.group(1)
+      result = evaluate(sub_clause)
+      new_expr = s.replace("(" + sub_clause + ")", str(result))
+      s = new_expr
+    else:
+      x = s.split(' ')
+      res = 0
+      while '+' in x:
+        i = x.index('+')
+        op1 = int(x.pop(i+1))
+        op = x.pop(i)
+        op2 = int(x[i-1])
+        x[i-1]=op1+op2
+      while len(x) > 1:
+        op1 = int(x.pop(0))
+        op  = x.pop(0)
+        op2 = int(x[0])
+        if op == '+':
+          res = op1 + op2
+        if op == '*':
+          res = op1 * op2
+        x[0] = res
+      return x[0]
+  return evaluate(s)
+    
+with open("18_input.txt") as f:
+  tot = 0
+  for l in f:
+    result = evaluate(l.strip('\n'))
+    print "RESULT: " + str(result)
+    tot += result
+
+print "\n\nTOTAL: " + str(tot)
